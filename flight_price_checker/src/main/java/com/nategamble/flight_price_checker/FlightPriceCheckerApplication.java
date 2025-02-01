706cb6161eb80a1adf5c465c0ef4cd19afbb3b2b
Nate Gamble <nate.eli.gamble@gmail.com>
2025-02-01 11:33:09 -0700

    Initialized Spring Boot project

---
+++ flight_price_checker/src/main/java/com/nategamble/flight_price_checker/FlightPriceCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.nategamble.flight_price_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPriceCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPriceCheckerApplication.class, args);
+	}
+
+}
---
+++ flight_price_checker/src/test/java/com/nategamble/flight_price_checker/FlightPriceCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nategamble.flight_price_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPriceCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
